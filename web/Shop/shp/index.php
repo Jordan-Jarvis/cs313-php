4c4308b756bb9f2d791135a12774e7a449613d47
jordan <jar17013@byui.edu>
2020-05-09 16:48:11 -0600

    php practice

--- web/Shop/shp/index.php
+++ web/Shop/shp/index.php
@@ -3,11 +3,11 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en"><head>
-<TITLE>PHP Shopping Cart without Database</TITLE>
+<TITLE>PHP Shopping Cart </TITLE>
 <link href="style.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
-<h1>Demo Shopping Cart without Database</h1>
+<h1>Shopping Cart </h1>
 <?php 
 require_once "product-gallery.php";
 ?>
@@ -15,9 +15,7 @@ require_once "product-gallery.php";
 <div id="shopping-cart">
 <div class="txt-heading">Shopping Cart <a id="btnEmpty" class="cart-action" onClick="cartAction('empty','');"><img src="images/icon-empty.png" /> Empty Cart</a></div>
 <div id="cart-item">
-<?php 
-require_once "ajax-action.php";
-?>
+
 </div>
 </div>
 <script src="jquery-3.2.1.min.js" type="text/javascript"></script>
