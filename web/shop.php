b3fed9431306d6067d6e28e0db047d204e3d73d4
jordan <jar17013@byui.edu>
2020-05-10 13:40:14 -0600

    php practice

--- web/shop.php
+++ web/shop.php
@@ -37,6 +37,32 @@ session_start();
 
         <input type="submit" name="Submit" value="Submit!" />
     </form>
+    <strong>Buy a camera:</strong>
+
+    <?php
+    $Dog = ["Poodle", "Pincer", "Foxhound"];
+    ?>        
+    <form action="" method="post">
+        <?php
+        if (isset($_POST['Submit'])) {
+            $_SESSION['camera'] = $_POST['Dog'];
+        }
+        ?>
+        <select name="camera">
+            <?php
+            foreach ($camera as $cam) {
+                $selected = "";
+                if (isset($_SESSION['camera']) && $_SESSION['camera'] == $cam) {
+                    $selected = "selected='selected'";
+                }
+                // select car based on session variable
+                echo "<option value='$cam' $selected>$cam</option>";
+            }
+            ?>
+        </select>
+
+        <input type="submit" name="Submit" value="Submit!" />
+    </form>
 
     <strong>
         <?php if (isset($_SESSION['camera'])) {
