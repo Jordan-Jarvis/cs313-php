d24552272a776ba9bf7c5fd14cfba57cc19ff47b
jordan <jar17013@byui.edu>
2020-05-10 15:01:56 -0600

    php practice

--- web/democart/cart.php
+++ web/democart/cart.php
@@ -4,14 +4,14 @@ session_start();
 <!doctype html public "-//w3c//dtd html 3.2//en">
 <html>
 <head>
-<title>Demo of Session array used for cart from plus2net.com</title>
+<title>Array init</title>
 </head>
 <body>
 
 <?Php
-$_SESSION['cart']=array(); // Declaring session array
-array_push($_SESSION['cart'],'apple','mango','banana'); // Items added to cart
-array_push($_SESSION['cart'],'Orange'); // Items added to cart
+$_SESSION['cart']=array(); 
+array_push($_SESSION['cart'],'Car','Dog','Cat');
+array_push($_SESSION['cart'],'Mouse'); 
 
 echo "Number of Items in the cart = ".sizeof($_SESSION['cart']);
 require 'menu.php';
