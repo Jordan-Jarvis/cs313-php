309ffee3736ab0d6548b0f47f6d27cde07935eec
jordan <jar17013@byui.edu>
2020-05-22 14:43:38 -0600

    Database read test

---
+++ web/week05/songs.php
@@ -0,0 +1,28 @@
+ 
+<?php
+session_start();
+
+    require_once 'database.php';
+    $db = get_db();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PGSQL Connect to DB</title>
+</head>
+<body>
+    <h1>Scripture Resources</h1>
+
+    <?php
+        $statement = $db->query('select title, a.album from song s join album a on s.album = a.id;');
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        {
+          echo '<p><b>' . $row['title'] . ' ' . $row['album'] . '</p>';
+        }
+    ?>
+</body>
+</html>
