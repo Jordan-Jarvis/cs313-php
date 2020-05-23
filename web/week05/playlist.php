fc4dd2d24e402b7867dbf1b7904922f612d6ee5d
jordan <jar17013@byui.edu>
2020-05-23 17:08:24 -0600

    Database read test

--- web/week05/playlist.php
+++ web/week05/playlist.php
@@ -26,8 +26,11 @@ session_start();
     ?>
     <h2>Second Playlist Query</h2>
     <?php
-        $statement = $db->query('select s.title from playlist p join songlist sl on p.songs = sl.list join song s on sl.songid = s.id where p.title = \'Second Playlist\' order by s.title;');
-        while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+        $stmt = $db->prepare('select s.title from playlist p join songlist sl on p.songs = sl.list join song s on sl.songid = s.id where p.title =:name order by s.title;');
+        $stmt->bindValue(':name', $name, PDO::PARAM_STR);
+        $stmt->execute();
+        $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
         {
           echo '<p><b>' . $row['title'] . '</p>';
         }
