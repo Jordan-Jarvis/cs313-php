1780edbd79c78487483b226b691a34d34a0fb0dd
jordan <jar17013@byui.edu>
2020-05-23 16:52:04 -0600

    Database read test

--- web/week05/playlist.php
+++ web/week05/playlist.php
@@ -21,6 +21,7 @@ session_start();
         $title = "First Playlist";
         $statement = $db->prepare('select s.title from playlist p join songlist sl on p.songs = sl.list join song s on sl.songid = s.id where p.title = \':title\' order by s.title;');
         $statement->bindValue(':title', $title, PDO::PARAM_STR);
+        $statement->execute();
         while ($row = $statement->fetch(PDO::FETCH_ASSOC))
         {
           echo '<p><b>' . $row['title'] . '</p>';
