c663ace57d7e6504bd683ea45d3eeed60cabcf97
jordan <jar17013@byui.edu>
2020-05-30 14:44:09 -0600

    Database added

--- web/addSongs.php
+++ web/addSongs.php
@@ -58,20 +58,7 @@ session_start();
     <form action="removeSong.php">
     <input type="submit" value="Remove a song" />
 </form>
-        <div class="item2">
-            <ul>
-                
-            <h2>Playlist Query</h2>
-                <?php
-
-                    $sqlQuery = "SELECT s.title from playlist p join songlist sl on p.songs = sl.list join song s on sl.songid = s.id where p.title ='$title' order by s.title;";
-                    foreach ($db->query($sqlQuery) as $row)
-                    {
-                        echo '<li>' . $row['title'] . '</li>';
-                    }
-                ?>
-            </ul>
-        </div>
+        
 
       <div class="item3">
           
